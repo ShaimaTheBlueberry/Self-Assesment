186eef1f9cdbbcb117467a73e097c5fd7def4456
zippy <blueberry.slurpee@gmail.com>
2017-06-02 17:16:06 -0700

    Update one.py

--- 3/one.py
+++ 3/one.py
@@ -2,4 +2,8 @@ introduction = ["I am shaima!", "I am awesome!", "I am in 5th grade", "!!!!"];
 def printer ( introduction ) :
     for s in introduction: 
         print s
+        
+def different ( introduction ) :
+    print (" ".join(introduction))
 printer(introduction);
+different(introduction);
